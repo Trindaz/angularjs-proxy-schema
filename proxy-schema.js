7391c134e741e25d3f84d3e2c3b95535e2506853
Trindaz <dave.trindall@gmail.com>
2014-09-21 15:59:13 -0400

    revert splitting to validate* functions which broke the example

--- proxy-schema.js
+++ proxy-schema.js
@@ -1,34 +1,8 @@
 
 var ProxySchema = function (modelName, Extends, schema) {
 
-  var validateChange = function(change) {
-    
-    //Warn when new properties added
-    if(change.type == 'add' && !schema[change.name]){
-      console.warn('Unknown member "' + change.name + '" added to ' + modelName + ' instance.');
-    }
-
-    //Warn when wrong types assigned (updating)
-    if(change.type == 'update' && schema[change.name].name != self[change.name].constructor.name){
-      console.warn('"' + change.name + '" in ' + modelName + ' instance updated to ' + self[change.name].constructor.name + ' instead of ' + schema[change.name].name) + '.';
-    }
-    
-    //Warn when wrong types assigned (adding)
-    if(change.type == 'add' && schema[change.name].name != self[change.name].constructor.name){
-      console.warn('"' + change.name + '" of type ' + self[change.name].constructor.name + ' instead of type ' + schema[change.name].name + ' added to ' + modelName + ' instance.');
-    }
-
-    if(change.type == 'delete' && schema[change.name]){
-      console.warn('Member "' + change.name + '" deleted from ' + modelName + ' instance.');
-    }
-
-  };
-
-  var validateChanges = function(changes) {
-    changes.forEach(validateChange);
-  };
-
   //TODO: implement modelName somehow instead of PmroxySchemaClass
+
   var ProxySchemaClass = function () {
     
     //TODO: Use console.warn when wrong types are used
@@ -36,22 +10,48 @@ var ProxySchema = function (modelName, Extends, schema) {
     var self = this;
 
     //Getter and setter checks on obj
-    Object.observe(self, validateChanges);
+    Object.observe(self, function (changes) {
+      changes.forEach(function (change) {
+        //console.log(change.type, change.name, change.oldValue);
+      
+        //Warn when new properties added
+        if(change.type == 'add' && !schema[change.name]){
+          console.warn('Unknown member "' + change.name + '" added to ' + modelName + ' instance.');
+        }
+
+        //Warn when wrong types assigned (updating)
+        if(change.type == 'update' && schema[change.name].name != self[change.name].constructor.name){
+          console.warn('"' + change.name + '" in ' + modelName + ' instance updated to ' + self[change.name].constructor.name + ' instead of ' + schema[change.name].name) + '.';
+        }
+        
+        //Warn when wrong types assigned (adding)
+        if(change.type == 'add' && schema[change.name].name != self[change.name].constructor.name){
+          console.warn('"' + change.name + '" of type ' + self[change.name].constructor.name + ' instead of type ' + schema[change.name].name + ' added to ' + modelName + ' instance.');
+        }
+
+        if(change.type == 'delete' && schema[change.name]){
+          console.warn('Member "' + change.name + '" deleted from ' + modelName + ' instance.');
+        }
+
+      });
+    });
     
     //Inherit from base class
     //TODO: Is it a problem that members are added to the child's __proto__ and not Resource's __proto__?
     Extends.apply(self, arguments);
     
-    //Add undefined placeholders for each member if not assigned by super constructor
+    //Add "stubs" for each member if not assigned by super constructor
     _.keys(schema).forEach(function (key) {
       if(key in self === false) {
         self[key] = undefined;
       }
     });
   
+
   };
 
   ProxySchemaClass.prototype = Extends.prototype;
+
   return ProxySchemaClass;
 
 };
@@ -63,10 +63,8 @@ angular.module('ProxySchemaApp', ['ngResource'])
       templateUrl: 'edit-example-entity.html',
       scope: {},
       link: function (scope) {
-
-        //Node: Default value types will be validated
-        scope.person = new PersonResource({ name: 'Johnny', age: 99});
-
+        //Note: Default value types will be validated
+        scope.person = new PersonResource({name: 'Johnny', age: 99});
       }
     }
   })
@@ -77,7 +75,6 @@ angular.module('ProxySchemaApp', ['ngResource'])
       update: {method: 'PUT'}
     });
 
-    //Define schema for "person"
     var Person = ProxySchema('Person', PersonResource, {
       name: String,
       age: Number
